eb3c7cee98932d3e943eb0812faf557490e8c589
Mitchell Mirano <mitchellmirano25@gmail.com>
2022-07-15 00:51:46 -0500

    the developers names

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>;
+#include <math.h>;
+
+using namespace std;
+
+
+
+
+
+int main(int argc, char const *argv[])
+{
+    cout<<"Un increible proyecto desarrollado por: Sebastian, Merly y Mitchell";
+    return 0;
+}
